f92bb7a74c66c215683d065152df4df10b754bd0
Dominik Lipinski <maboonga@gmail.com>
2017-06-29 21:37:14 +0200

    split code html/js/css (before bootstrap)

---
+++ generator.js
@@ -0,0 +1,26 @@
+	function makeStatement(){
+		var part1 =["Kaczyński", "Hoffman", "Macierewicz", "Brudziński", "Ziobro"];
+		var part2 =["swoim seansem nienawiści", "podczas tego obłędnego spędu", "tam, na Krakowskim Przemieściu",
+		"mówiąc te dyrdymały, bzdury i wypociny", "oczerniając i obrzucając błotem premiera"];
+		var part3 =["pokazał hamstwo i bezczelność", "przekroczył granicę zbydlęcenia", "szkodzi dobremu wizerunkowi Polski", "podnosi rękę na demokratyczny rząd", "zachowuje się jak nazista"];
+		var part4 =["prowadząc do wojny domowej.", "pokazując swe prostactwo moralne.", "podpalając Polskę i dzieląc Polaków.", "-powinien trafić do szpitala psychiatrycznego.", "schodząc poniżej poziomu dna wyznacznonego przez Kępę."];
+	
+		var rand1 = Math.floor(Math.random()*part1.length);
+		var rand2 = Math.floor(Math.random()*part2.length);
+		var rand3 = Math.floor(Math.random()*part3.length);
+		var rand4 = Math.floor(Math.random()*part4.length);
+
+		var awesomeStatement = part1[rand1] +" "+ part2[rand2]+" "+ part3[rand3]+" "+part4[rand4];
+		alert(awesomeStatement);
+	}
+	makeStatement();
+
+// <!-- add html/onclick/print on page instead of alert
+// https://mfiles.pl/pl/index.php/Uniwersalny_kod_przem%C3%B3wie%C5%84
+// http://bi.gazeta.pl/im/00/9c/12/z19515648P,Tabelka.jpg
+// http://www.lasypolskie.pl/files/thumbs/t_tr1_110.jpg
+// http://joemonster.org/phorum/read.php?f=18&t=561350
+// http://niezalezna.pl/68262-ty-tez-mozesz-byc-jak-ewa-kopacz-generator-przemowien-premier-robi-furore-w-sieci
+// http://grudqowy.salon24.pl/68864,tabelka-do-tworzenia-wypowiedzi-medialnych-dla-politykow-i-zwole
+// https://twitter.com/marekborowski/status/690917521571123201
+//  -->
