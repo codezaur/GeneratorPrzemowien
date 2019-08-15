98d6fd30ce92fdc0b4a7c0c882bf914bc52ddf61
DominikLipinski <maboonga@gmail.com>
2019-08-15 20:33:46 +0200

    updated for ssl

--- generator.js
+++ generator.js
@@ -3,7 +3,7 @@
 var faceSound = new Audio("picsound.wav"),
 	faceToClick = document.querySelectorAll("#faces");
 
-var url = "http://syntaxerror2055.eu/GeneratorPrzemowien/statementsParts.json";
+var url = "https://schrodingerscode.net/GeneratorPrzemowien/statementsParts.json";
 var request = new XMLHttpRequest();
 request.onload = requestListener();
 request.open("GET", url);
@@ -11,44 +11,44 @@ request.send(null);
 
 function requestListener() {
 	function makeNiesiolStatement(){
-		
+
 		var statements = JSON.parse(request.responseText),
 			awesomeStatement = [];
-		
-		for (var i=0; i< 7; i++){ 
+
+		for (var i=0; i< 7; i++){
 
 		awesomeStatement += " "+statements.part1[Math.floor(Math.random()*statements.part1.length)];
 		awesomeStatement += " "+statements.part2[Math.floor(Math.random()*statements.part2.length)];
 		awesomeStatement += " "+statements.part3[Math.floor(Math.random()*statements.part3.length)];
 		awesomeStatement += " "+statements.part4[Math.floor(Math.random()*statements.part4.length)];
-		
+
 		if(i==2||i==5){
 			awesomeStatement += " " +"</p>";
-		}	
-		}	
+		}
+		}
 		awesomeStatement += " "+"Poza tym kiedyś jedzono szczaw i wszyscy byli najedzeni."
 
-		document.querySelector(".wisdom-container").style.display  = "block";				
+		document.querySelector(".wisdom-container").style.display  = "block";
 		document.getElementById("wisdom").innerHTML = awesomeStatement;
 	}
 
 	function makeGomulkaStatement(){
-	
+
 		var statements = JSON.parse(request.responseText),
 			awesomeStatement = [];
-		
-		for (var i=0; i< 7; i++){ 
+
+		for (var i=0; i< 7; i++){
 
 		awesomeStatement += " "+statements.part5[Math.floor(Math.random()*statements.part5.length)];
 		awesomeStatement += " "+statements.part6[Math.floor(Math.random()*statements.part6.length)];
 		awesomeStatement += " "+statements.part7[Math.floor(Math.random()*statements.part7.length)];
 		awesomeStatement += " "+statements.part8[Math.floor(Math.random()*statements.part8.length)];
-		
+
 		if(i==2||i==5){
 			awesomeStatement += " " +"</p>";
 		}
 		}
-		
+
 		document.querySelector(".wisdom-container").style.display  = "block";
 		document.getElementById("wisdom").innerHTML = awesomeStatement;
 	}
@@ -57,19 +57,19 @@ function requestListener() {
 
 		var statements = JSON.parse(request.responseText),
 			awesomeStatement = [];
-		
-		for (var i=0; i< 7; i++){ 
+
+		for (var i=0; i< 7; i++){
 
 		awesomeStatement += " "+statements.part9[Math.floor(Math.random()*statements.part9.length)];
 		awesomeStatement += " "+statements.part10[Math.floor(Math.random()*statements.part10.length)];
 		awesomeStatement += " "+statements.part11[Math.floor(Math.random()*statements.part11.length)];
 		awesomeStatement += " "+statements.part12[Math.floor(Math.random()*statements.part12.length)];
-		
+
 		if(i==2||i==5){
 			awesomeStatement += " " +"</p>";
 		}
 		}
-						
+
 		document.querySelector(".wisdom-container").style.display  = "block";
 		document.getElementById("wisdom").innerHTML = awesomeStatement;
 	}
@@ -86,9 +86,9 @@ faceToClick[2].addEventListener("click", makePISStatement);
 }
 
 }
-})()
+})();
 
-// Speech sources: 
+// Speech sources:
 // https://mfiles.pl/pl/index.php/Uniwersalny_kod_przem%C3%B3wie%C5%84
 // http://bi.gazeta.pl/im/00/9c/12/z19515648P,Tabelka.jpg
 // http://joemonster.org/phorum/read.php?f=18&t=561350
